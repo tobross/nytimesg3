fb75b46df2b5f049b756d8b14b28813fa5d8e494
erikamats <erikamats@hotmail.com>
2018-04-27 12:47:08 -0500

    getting new updates

--- assets/javascript/Group-exercise-data.js
+++ assets/javascript/Group-exercise-data.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-console.log("document test");
 
-=======
-  console.log(document);
->>>>>>> 0034381cbe8f60162463218300358f2ee81b654d
 var searchTerm = "Willie Nelson";
 var beginDate = "20180101";
 var endDate = "20180331";
@@ -22,17 +17,11 @@ url += '?' + $.param({
 
 var url;
 var apiKey = "ee0166e4d7ba467f90d3fb4b44d0e803"
-<<<<<<< HEAD
 
 $(".snarfyboo").on("click", function(event){ 
-=======
-$(".snarfyboo").on("click", function(e) {
-  console.log(e);
-  e.preventDefault();
->>>>>>> 0034381cbe8f60162463218300358f2ee81b654d
-  searchTerm = $("#searchTerm", value);
-  beginDate = $("#beginDate", value);
-  endDate = $("#endDate", value);
+  searchTerm = $("#searchTerm").val());
+  beginDate = $("#beginDate").val();
+  endDate = $("#endDate").val();
   url = "https://api.nytimes.com/svc/v2/articlesearch.json?api-key==" + apiKey + "&q=" + searchTerm + "&begin_date=" + startDate + "&end_date=" + endDate 
   console.log(document);
   event.preventDefault();
@@ -40,7 +29,6 @@ $(".snarfyboo").on("click", function(e) {
 $.ajax({
   url: queryURL,
   method: 'GET',
-<<<<<<< HEAD
 })
 .then(function(result) {
   console.log(result);
@@ -51,11 +39,3 @@ $.ajax({
 // function formClear() {
 //   document.getElementById(".clear").reset();
 // }
-=======
-}).then(function(result) {
-  console.log(result);
-});
-});
-
-});
->>>>>>> 0034381cbe8f60162463218300358f2ee81b654d
