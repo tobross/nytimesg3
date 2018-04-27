abce8f8d1d3b5fc0d6b8c866a349163de93c3b47
erikamats <erikamats@hotmail.com>
2018-04-27 10:44:53 -0500

    push2

--- assets/javascript/Group-exercise-data.js
+++ assets/javascript/Group-exercise-data.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 var searchTerm = "Willie Nelson";
 var beginDate = "20180101";
 var endDate = "20180331";
-<<<<<<< HEAD
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
@@ -15,7 +14,6 @@ url += '?' + $.param({
 
 
 
-=======
 var url;
 var apiKey = "ee0166e4d7ba467f90d3fb4b44d0e803"
 $(".search").on("click", function() {
@@ -24,7 +22,6 @@ $(".search").on("click", function() {
   endDate = $("#endDate", value);
   url = "https://api.nytimes.com/svc/v2/articlesearch.json?api-key==" + apiKey + "&q=" + searchTerm + "&begin_date=" + startDate + "&end_date=" + endDate 
   console.log(document);
->>>>>>> 25014f15408d407d94e8fc03afa359baadc8fc35
 $.ajax({
   url: url,
   method: 'GET',
@@ -33,12 +30,8 @@ $.ajax({
 }).fail(function(err) {
   throw err;
 });
-<<<<<<< HEAD
 
 function formClear() {
   document.getElementById("
   #form-group").reset();
 }
-=======
-})});
->>>>>>> 25014f15408d407d94e8fc03afa359baadc8fc35
