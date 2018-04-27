0034381cbe8f60162463218300358f2ee81b654d
Tobey Ross <tobross@gmail.com>
2018-04-27 11:34:03 -0500

    added preventDefault

--- assets/javascript/Group-exercise-data.js
+++ assets/javascript/Group-exercise-data.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  console.log(document);
 var searchTerm = "Willie Nelson";
 var beginDate = "20180101";
 var endDate = "20180331";
@@ -16,22 +17,20 @@ url += '?' + $.param({
 
 var url;
 var apiKey = "ee0166e4d7ba467f90d3fb4b44d0e803"
-$(".search").on("click", function() {
+$(".snarfyboo").on("click", function(e) {
+  console.log(e);
+  e.preventDefault();
   searchTerm = $("#searchTerm", value);
   beginDate = $("#beginDate", value);
   endDate = $("#endDate", value);
   url = "https://api.nytimes.com/svc/v2/articlesearch.json?api-key==" + apiKey + "&q=" + searchTerm + "&begin_date=" + startDate + "&end_date=" + endDate 
   console.log(document);
 $.ajax({
-  url: url,
+  url: queryURL,
   method: 'GET',
-}).done(function(result) {
+}).then(function(result) {
   console.log(result);
-}).fail(function(err) {
-  throw err;
+});
 });
 
-function formClear() {
-  document.getElementById("
-  #form-group").reset();
-}
+});
